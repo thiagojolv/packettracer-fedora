960adc659c6dafcd99e39d9aaf3d32071a4d7e0c
Thiago Jack <thiagoojack@gmail.com>
2021-12-15 01:59:46 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Cisco Packet Tracer 8.1.0 on Fedora 35
+
+Install Cisco Packet Tracer 8.1.0 Easily on Fedora 35.
+
+Follow this steps to easily install:
+- Clone this repo to your system;
+- Put the Cisco Packet Tracer .deb file on localy cloned repo (yes, along with _install.sh_ and _cisco-pt.desktop_);
+- Run `sudo bash install.sh` and relax ;D
---
+++ install.sh
@@ -0,0 +1,34 @@
+if [ -e /opt/pt ]; then
+  echo "Removing old version of Packet Tracer from /opt/pt"
+  sudo rm -rf /opt/pt
+  sudo rm -rf /usr/share/applications/cisco-pt.desktop
+  sudo rm -rf /usr/share/applications/cisco-ptsa.desktop
+  sudo rm -rf /usr/share/applications/cisco7-pt.desktop
+  sudo rm -rf /usr/share/applications/cisco7-ptsa.desktop
+  sudo xdg-desktop-menu uninstall /usr/share/applications/cisco-pt.desktop
+  sudo xdg-desktop-menu uninstall /usr/share/applications/cisco-ptsa.desktop
+  sudo update-mime-database /usr/share/mime
+  sudo gtk-update-icon-cache --force /usr/share/icons/gnome
+  
+  sudo rm -f /usr/local/bin/packettracer
+fi
+
+ar -x CiscoPacketTracer_810_Ubuntu_64bit.deb
+tar -xvf control.tar.xz
+tar -xvf data.tar.xz
+
+mv cisco-pt.desktop usr/share/applications/cisco-pt.desktop
+sudo cp -r usr /
+sudo cp -r opt /
+sudo ./postinst
+
+sudo dnf install -y \
+  qt5-qtmultimedia.x86_64 \
+  qt5-qtwebengine.x86_64 \
+  qt5-qtnetworkauth.x86_64 \
+  qt5-qtwebsockets.x86_64 \
+  qt5-qtwebchannel.x86_64 \
+  qt5-qtscript.x86_64 \
+  qt5-qtlocation.x86_64 \
+  qt5-qtsvg.x86_64 \
+  qt5-qtspeech
