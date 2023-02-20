37813d5d17b24952c828ba6d0a30bdcf6366e3f6
Thiago Jack <thiagoojack@gmail.com>
2023-02-20 09:58:40 -0300

    Code improvements This commit add improvements suggested by Ricky-Tigg. - Code compacted on uninstall script; - Substitute broken download link; - Proper use of `sudo` in the installation script; - and others little improvements.
    Co-authored-by: Ricky-Tigg <ricky.tigg@gmail.com>

--- install.sh
+++ install.sh
@@ -7,24 +7,14 @@ installer_name_3=Cisco*.deb
 path_to_pt=$(find /home -name $installer_name_1 -o -name $installer_name_2 -o -name $installer_name_3)
 
 if [[ -z "$path_to_pt" ]]; then
-    echo "Packet Tracer installer not found in /home. It must be named like this: $installer_name."
-    echo "You can download the installer from www.netacad.com/portal/node/488."
+    echo "Packet Tracer installer not found in /home. It must be named like this: $installer_name_1."
+    echo "You can download the installer from https://www.netacad.com/portal/resources/packet-tracer"
+    echo "or https://skillsforall.com/resources/lab-downloads (login required)."
     exit 1
 fi
 
 if [ -e /opt/pt ]; then
-  echo "Removing old version of Packet Tracer from /opt/pt"
-  sudo rm -rf /opt/pt
-  sudo rm -rf /usr/share/applications/cisco-pt.desktop
-  sudo rm -rf /usr/share/applications/cisco-ptsa.desktop
-  sudo rm -rf /usr/share/applications/cisco7-pt.desktop
-  sudo rm -rf /usr/share/applications/cisco7-ptsa.desktop
-  sudo xdg-desktop-menu uninstall /usr/share/applications/cisco-pt.desktop
-  sudo xdg-desktop-menu uninstall /usr/share/applications/cisco-ptsa.desktop
-  sudo update-mime-database /usr/share/mime
-  sudo gtk-update-icon-cache --force /usr/share/icons/gnome
-
-  sudo rm -f /usr/local/bin/packettracer
+  sudo bash ./uninstall.sh
 fi
 
 echo "Extracting files"
@@ -36,18 +26,10 @@ tar -xvf packettracer/data.tar.xz --directory=packettracer
 sudo cp -r packettracer/usr /
 sudo cp -r packettracer/opt /
 sudo sed -i 's/packettracer/packettracer --no-sandbox args/' /usr/share/applications/cisco-pt.desktop
+sudo sed -i 's/sudo xdg-mime/sudo -u $SUDO_USER xdg-mime/' ./packettracer/postinst 
 sudo ./packettracer/postinst
 
 echo "Installing dependencies"
-sudo dnf install -y \
-  qt5-qtmultimedia.x86_64 \
-  qt5-qtwebengine.x86_64 \
-  qt5-qtnetworkauth.x86_64 \
-  qt5-qtwebsockets.x86_64 \
-  qt5-qtwebchannel.x86_64 \
-  qt5-qtscript.x86_64 \
-  qt5-qtlocation.x86_64 \
-  qt5-qtsvg.x86_64 \
-  qt5-qtspeech
+sudo dnf -y install qt5-qt{multimedia,webengine,networkauth,websockets,webchannel,script,location,svg,speech}
 
 sudo rm -rf packettracer
